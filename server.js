24e16c65535dbb5952f3b363ff48efce175ea7b7
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-11-20 22:33:39 -0300

    organizando instuções em pastas individuais

--- server.js
+++ server.js
@@ -1,43 +1,9 @@
 import express from "express";
-
-
-const posts = [
-    {
-        id: 1,
-        descricao: "foto do gatinho",
-        imgagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 2,
-        descricao: "Paisagem de montanha",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 3, 
-        descricao: "Cachorro brincando",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-
-function buscarPostPorId (id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-};
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id)
-    res.status(200).json(posts[index]);
 });
