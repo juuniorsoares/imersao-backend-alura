aad9132e8ec65ce5ced190e44038d64c4bacdd73
juuniorsoares <139187475+juuniorsoares@users.noreply.github.com>
2024-11-22 23:21:57 -0300

    atualizações na aplicação

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,24 @@
 import express from "express";
-import { listarPosts } from "../controller/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPosto, uploadImagem } from "../controller/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
+
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPosto);
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 };
 
 export default routes;
